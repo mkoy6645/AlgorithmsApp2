c56b407c1d6bd31d5b58297c179775d4e65b79a0
mkoy6645 <70230000+mkoy6645@users.noreply.github.com>
2020-09-22 11:43:46 -0600

    created gitignore

---
+++ AlgorithmsApp/Controller/AlgorithmView.swift
@@ -0,0 +1,23 @@
+//
+//  AlgorithmView.swift
+//  AlgorithmsApp
+//
+//  Created by Koyle, Michael on 9/18/20.
+//  Copyright © 2020 CTEC. All rights reserved.
+//
+
+import SwiftUI
+
+NavigationView
+{
+    VStack
+    {
+        Text("Placeholder")
+        Image("placeholder")
+        Text("Placeholder")
+    
+    NavigationLink("Placeholder" destination: Navdemo))
+}
+
+
+}
---
+++ AlgorithmsApp/Controller/IntroView.swift
@@ -0,0 +1,61 @@
+//  Created by Koyle, Michael on 9/18/20.
+//  Copyright © 2020 CTEC. All rights reserved.
+
+
+import SwiftUI
+
+
+NavigationView
+{
+    VStack
+    {
+        Text("Introduction to algorithms in CSP")
+        Image("Code Example 66% @2x")
+        
+    
+    NavigationLink("Placeholder" destination: Navdemo))
+}
+
+    let algorithmTitle : String "Code Alogrithm"
+    
+}
+
+private func setupAlgorithm -> String
+{
+    let algorithmTitle : String = "How to dance"
+    
+    let stepOne : String = "First you do asdf "
+    let stepTwo : String = "First you do asdf "
+    let stepThree : String = "First you do asdf "
+    let stepFour : String = "First you do asdf "
+    
+    let algorithm = [stepOne, stepTwo, stepThree, stepFour]
+    
+    var algorithmDescription = algorithmTitle
+    
+    for step in algorithm
+    {
+        let bullet : String = "sd"
+        let bulletedStep : String = "\n \(bullet) \(step)"
+        
+        algorithmDescription.append(bulletedStep)
+        
+    }
+    return algorithmDescription
+        
+    
+    VStack
+    {
+        Text(algorithmText)
+            .bold()
+        Image("my algorithm")
+            .resizable()
+            .scaledToFit()
+        Text(setupAlgorithm())
+            .rotation3DEffect(Angle(degrees: 45),
+                              axis: (x : 1.0, y: 0.0, z: 0.0))
+        NavigationLink("Switch to Intro View", destination: IntroView())
+    }
+    
+    
+}
